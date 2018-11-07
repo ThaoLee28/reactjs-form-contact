c2d7ceb06e63807790149b55e26ac9ae0d4af10a
Thao Le <thuthao.le2809@gmail.com>
2018-11-07 20:28:35 +0700

    change layout

--- src/components/FormContact.js
+++ src/components/FormContact.js
@@ -68,139 +68,161 @@ class FormContact extends Component {
                   }
                 }}
               />
-              <div className="flex flex-wrap my-3">
-                <Field name="companyName">
-                  {({ input }) => (
-                    <div className="flex-1 mx-5">
-                      <label 
-                        className="block mb-2"
-                      >
-                        Your company or project name
-                      </label>
-                      <input 
-                        {...input} 
-                        type="text"
-                        className="block w-full bg-grey-lighter border border-solid rounded py-3 px-4 mb-3 leading-tight 
-                        focus:outline-none focus:bg-white focus:border-blue"
-                      />
-                    </div>
-                  )}
-                </Field>
-                <Field name="name">
-                  {({ input }) => (
-                    <div className="flex-1 mx-5">
-                      <label 
-                        className="block mb-2"
-                      >
-                        What is your name?
-                      </label>
-                      <input 
-                        {...input} 
-                        type="text"
-                        className="block w-full bg-grey-lighter border border-solid rounded py-3 px-4 mb-3 leading-tight
-                        focus:outline-none focus:bg-white focus:border-blue"
-                      />
-                    </div>
-                  )}
-                </Field>
+              <div className="row md:flex">
+                <div className="col w-1/2 mx-2">
+                  <Field name="companyName">
+                    {({ input }) => (
+                      <div className="mb-5">
+                        <label 
+                          className="block mb-2"
+                        >
+                          Your company or project name
+                        </label>
+                        <input 
+                          {...input} 
+                          type="text"
+                          className="block w-full bg-grey-lighter border border-solid rounded py-3 px-4 mb-3 leading-tight 
+                          focus:outline-none focus:bg-white focus:border-blue"
+                        />
+                      </div>
+                    )}
+                  </Field>
+                </div>
+                <div className="col w-1/2 mx-2">
+                  <Field name="name">
+                    {({ input }) => (
+                      <div className="mb-5">
+                        <label 
+                          className="block mb-2"
+                        >
+                          What is your name?
+                        </label>
+                        <input 
+                          {...input} 
+                          type="text"
+                          className="block w-full bg-grey-lighter border border-solid rounded py-3 px-4 mb-3 leading-tight
+                          focus:outline-none focus:bg-white focus:border-blue"
+                        />
+                      </div>
+                    )}
+                  </Field>
+                </div>
               </div>
-              <div className="flex flex-wrap my-3">
-                <Field name="email">
-                  {({ input, meta }) => (
-                    <div className="flex-1 mx-5">
-                      <label 
-                        className="block mb-2"
-                      >
-                        What is your email address? 
-                        <span className="text-red"> *</span>
-                      </label>
-                      <input 
-                        {...input} 
-                        type="text"
-                        className="block w-full bg-grey-lighter border border-solid rounded py-3 px-4 mb-3 leading-tight
-                        focus:outline-none focus:bg-white focus:border-blue"
+              <div className="row md:flex">
+                <div className="col w-1/2 mx-2">
+                  <Field name="email">
+                    {({ input, meta }) => (
+                      <div className="mb-5">
+                        <label 
+                          className="block mb-2"
+                        >
+                          What is your email address? 
+                          <span className="text-red"> *</span>
+                        </label>
+                        <input 
+                          {...input} 
+                          type="text"
+                          className="block w-full bg-grey-lighter border border-solid rounded py-3 px-4 mb-3 leading-tight
+                          focus:outline-none focus:bg-white focus:border-blue"
+                        />
+                        {meta.error && meta.touched && <span className="text-red">{meta.error}</span>}
+                      </div>
+                    )}
+                  </Field>
+                </div>
+                <div className="col w-1/2 mx-2">
+                  <Field name="phone">
+                    {({ input }) => (
+                      <div className="mb-5">
+                        <label 
+                          className="block mb-2"
+                        >
+                          What is your phone number?
+                        </label>
+                        <input 
+                          {...input} 
+                          type="text"
+                          className="block w-full bg-grey-lighter border border-solid rounded py-3 px-4 mb-3 leading-tight
+                          focus:outline-none focus:bg-white focus:border-blue"
+                        />
+                      </div>
+                    )}
+                  </Field>
+                </div>
+              </div>
+              <div className="row md:flex">
+                <div className="col w-full mb-5 mx-2">
+                  <label className="block mb-2">Which location is closest to you?</label>
+                  <Field name="location">
+                    {({ input }) => (
+                      <RadioGroup
+                        items= {[
+                          { name: 'USA', id: 1 },
+                          { name: 'Europe', id: 2 },
+                          { name: 'Singapore', id: 3 },
+                          { name: 'Orther', id: 4 },
+                        ]}
+                        onChange={input.onChange}
                       />
-                      {meta.error && meta.touched && <span className="text-red">{meta.error}</span>}
-                    </div>
-                  )}
-                </Field>
-                <Field name="phone">
-                  {({ input }) => (
-                    <div className="flex-1 mx-5">
-                      <label 
-                        className="block mb-2"
-                      >
-                        What is your phone number?
-                      </label>
-                      <input 
-                        {...input} 
-                        type="text"
-                        className="block w-full bg-grey-lighter border border-solid rounded py-3 px-4 mb-3 leading-tight
-                        focus:outline-none focus:bg-white focus:border-blue"
-                      />
-                    </div>
-                  )}
-                </Field>
+                    )}
+                  </Field>
+                </div>
               </div>
-              <div className="my-3 mx-5">
-                <label className="block mb-2">Which location is closest to you?</label>
-                <Field name="location">
-                  {({ input }) => (
-                    <RadioGroup
-                      items= {[
-                        { name: 'USA', id: 1 },
-                        { name: 'Europe', id: 2 },
-                        { name: 'Singapore', id: 3 },
-                        { name: 'Orther', id: 4 },
-                      ]}
-                      onChange={input.onChange}
-                      optionItem={input.value.name}
-                    />
-                  )}
-                </Field>
+              <div className="row md:flex">
+                <div className="relative col w-full mb-5 mx-2">
+                  <label className="block mb-2">What is your budget?</label>
+                  <Field 
+                    name="budget" 
+                    component="select"
+                    className="cursor-pointer block w-full border border-solid h-12 bg-grey-lighter focus:outline-none px-4 py-3 appearance-none"
+                  >
+                    <option value="not define">Not define</option>
+                    <option value="up to $15,000">Up to $15,000</option>
+                    <option value="up to $30,000">Up to $30,000</option>
+                    <option value="up to $60,000">Up to $60,000</option>
+                    <option value="up to $100,000">Up to $100,000</option>
+                    <option value="more than $100,000">More than $100,000</option>
+                  </Field>
+                  <SvgDropdown 
+                    className="absolute pin-r" 
+                    style={{ top: `50%`, transform: `translateX(-50%)` }}
+                  />
+                </div>
               </div>
-              <div className="relative my-3 mx-5">
-                <label className="block mb-2">What is your budget?</label>
-                <Field 
-                  name="budget" 
-                  component="select"
-                  className="cursor-pointer block w-full border border-solid h-12 bg-grey-lighter focus:outline-none px-4 py-3 appearance-none"
-                >
-                  <option value="not define">Not define</option>
-                  <option value="up to $15,000">Up to $15,000</option>
-                  <option value="up to $30,000">Up to $30,000</option>
-                  <option value="up to $60,000">Up to $60,000</option>
-                  <option value="up to $100,000">Up to $100,000</option>
-                  <option value="more than $100,000">More than $100,000</option>
-                </Field>
-                <SvgDropdown className="absolute pin-r" style={{ top: `50%`, transform: `translateX(-50%)` }}/>
-              </div>
-              <div className="relative my-3 mx-5">
-                <label className="block mb-2">Tell us about your project</label>
-                <Field
-                  name="about"
-                  component="textarea"
-                  className="block border border-solid bg-grey-lighter w-full focus:outline-none px-4 py-3"
-                  rows={5}
-                >
-                </Field>
+              <div className="row md:flex">
+                <div className="relative col w-full mb-5 mx-2">
+                  <label className="block mb-2">Tell us about your project</label>
+                  <Field
+                    name="about"
+                    component="textarea"
+                    className="block border border-solid bg-grey-lighter w-full focus:outline-none px-4 py-3"
+                    rows={5}
+                  >
+                  </Field>
 
-                <label htmlFor="upload">
-                  <SvgAttract 
-                    className="absolute pin-t pin-r mr-2" style={{ top: `25%`, transform: `translateX(-25%)` }}
-                  /> 
-                </label>
-                <Field 
-                  name="upload_file"
-                  component="input"
-                  type="file"
-                  id="upload"
-                  className="hidden"
-                  onChange={this.handleselectedFile}
-                >
-                </Field>
+                  <label htmlFor="upload">
+                    <SvgAttract 
+                      className="absolute pin-t pin-r mr-2" 
+                      style={{ top: `25%`, transform: `translateX(-25%)` }}
+                    /> 
+                  </label>
+                  <Field 
+                    name="upload_file"
+                    component="input"
+                    type="file"
+                    id="upload"
+                    className="hidden"
+                    onChange={this.handleselectedFile}
+                  >
+                  </Field>
+                </div>
               </div>
-              <div className="my-3 mx-5 flex justify-center item-center">
+              {/* <ul className="list-reset">
+                <li>
+                  <span>{this.state.selectedFile[0].name}</span>
+                </li>
+              </ul> */}
+              <div className="row mx-2 flex justify-center item-center">
                 <button 
                   type="submit" 
                   disabled={submitting} 
